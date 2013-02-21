dc4d307bff52095b05a060d5aaafbafea4633c30
Daniel Dadap <daniel@dadap.net>
2013-02-20 20:46:15 -0600

    Also generate previews

--- common.mk
+++ common.mk
@@ -3,9 +3,9 @@ SRC := "."
 endif
 
 %.pdf: %.ly $(SRC)/template.ly $(SRC)/common.mk
-	lilypond -dinclude-settings="$(SRC)/template.ly" $<
+	lilypond -dpreview -dinclude-settings="$(SRC)/template.ly" $<
 
 .PHONY: clean
 
 clean:
-	$(RM) *.pdf
+	$(RM) *.eps *.pdf *.png
